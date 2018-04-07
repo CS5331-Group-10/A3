68d8d15906b3643da985935c159de1f35dfdbecc
zishan <zishan_lee@alumni.sutd.edu.sg>
2018-04-07 10:11:20 +0800

    trying to scrape

--- tutorial/tutorial/spiders/example.py
+++ tutorial/tutorial/spiders/example.py
@@ -1,11 +1,29 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+from scrapy.spiders import BaseSpider
+from scrapy.selector import Selector
+from tutorial.items import TutorialItem
+from scrapy.http import Request
+
 
 
 class ExampleSpider(scrapy.Spider):
-    name = 'crawler example'
-    allowed_domains = ['http://target.com/']
+    name = 'crawler_assignment'
+    allowed_domains = ['target.com/']
     start_urls = ['http://target.com/']
 
     def parse(self, response):
-        pass
+        hxs =  Selector(response=response)
+        visited_links = []
+        # links = hxs.path('//a/@href').extract()
+        links = hxs.xpath('//a/@href').extract()
+        link_validator = re.compile("^(?:http|https)")
+        for link in links:
+            if link_validator.match(link) and not link in visited_links:
+                visited_links.append(link)
+                yield Request(link,self.parse)
+            else:
+                full_url = response.urljoin(link)
+                visited_links.append(full_url)
+                yield Request(full_url, self.parse)
