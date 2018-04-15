669c0ea0e52f1df17b05ac18fa200824b7af1dc3
weeyou90 <weeyou90@gmail.com>
2018-04-15 16:37:39 +0800

    added some instructions in main.py

--- main.py
+++ main.py
@@ -6,11 +6,9 @@ json_data = open("result.json","r")
 data = json.load(json_data)
 
 
-#for each endpoint in JSON, get ENDPOINT, PARAMS, METHOD
-	#for each payload,
-		#run injectPayload(url, paramname, method, payload) [ALREADY IMPORTED]	
-		#currently, this code prints out the payload if successful, and writes a 		dummy .sh file [probably buggy at this point]
+#for each endpoint in JSON, get ENDPOINT, PARAMS, METHOD [TBC: Zishan]
+	#for each payload, [TBC: Zhizhong]
+		#run injectPayload(url, paramname, method, payload) [DONE]	
+		#currently, this code prints out the payload if successful, and writes a 		dummy .sh file [TBC: Jason]
 
-
-
-#To be done: consolidate result in a .JSON file
+#Once above is all completed, consolidate result in a .JSON file [TBC: WY]
