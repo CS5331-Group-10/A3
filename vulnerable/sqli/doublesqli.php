e4f4ba59ede6ad3fbf6ac06b47c8c2f0d6fa0a04
weeyou90 <weeyou90@gmail.com>
2018-04-21 18:14:31 +0800

    add gitignore

---
+++ vulnerable/sqli/doublesqli.php
@@ -0,0 +1,12 @@
+<html>
+
+<body>
+<h1>User Directory</h1>
+
+Search for user:
+<form id="directory" action="doublesqliaction.php" method="POST">
+    <input type="text" name="username" />
+    <input type="submit" />
+</form>
+</body>
+</html>
