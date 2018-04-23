873eb4d059bf73bf26debb19b785067e33258e50
Jason <swkjason@hotmail.com>
2018-04-24 01:14:41 +0800

    update setup.sh for splash

--- setup.sh
+++ setup.sh
@@ -7,4 +7,21 @@ sudo pip install --upgrade pip
 sudo pip install requests
 sudo apt install python-scrapy
 
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install docker-ce
+
+docker run -p 8050:8050 scrapinghub/splash
+
 # echo "cs5331 Y Y Y Y Y" | ./setup.sh
