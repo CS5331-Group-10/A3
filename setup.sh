c90ed84c7446f5f18bc6776f40f3397e7742326e
Jason <swkjason@hotmail.com>
2018-04-23 23:01:21 +0800

    created setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt install git
+#git clone https://github.com/CS5331-Group-10/A3.git
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install requests
+sudo apt install python-scrapy
+
+# echo "cs5331 Y Y Y Y Y" | ./setup.sh
